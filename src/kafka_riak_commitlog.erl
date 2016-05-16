a2d7095406e41d6869cb3db86f68873caec83dc0
Stephen Ball <sdball@gmail.com>
2016-05-16 12:52:05 -0400

    Reduce scope of the hook: only forward to OTP
    Instead of doing all the work in a single Erlang function we're adding
    an OTP application that will handle the bulk of the effort to reliably
    produce to Kafka.
    
    Rather than integrate an OTP application into the running Riak node and
    Erlang version (which has proven difficult and complex) this hook will
    now only send the action, bucket, key, and value from the Riak object to
    a separate OTP application.
    
    The only tricky part of this hook is then to know where to send the Riak
    object data. Currently it's doing a naive transformation of its own node
    name and expecting the external OTP service to be separately running.

--- src/kafka_riak_commitlog.erl
+++ src/kafka_riak_commitlog.erl
@@ -1,63 +1,54 @@
 -module(kafka_riak_commitlog).
--export([log/1, produce_to_kafka/2]).
+-export([postcommit_hook/1, call_to_remote_node/4, cast_to_remote_node/4]).
 
--define(TOPIC, <<"commitlog">>).
--define(PARTITION, 0).
--define(CLIENTNAME, brod_client_1).
+-define(OTP_PROCESS, kafka_riak_commitlog).
 
-log(Object) ->
-  Key = get_key(Object),
-  Bucket = get_bucket(Object),
-  try
-    {ok, Message} = build_kafka_message(Bucket, Key, Object),
-    {Timing, ok} = timer:tc(?MODULE, produce_to_kafka, [Key, Message]),
-    log_output(Bucket, Key, ok, Timing)
-  catch
-    Type:Exception ->
-      log_output(Bucket, Key, Type, Exception),
-      throw(Exception)
-  end.
+%% ---------------------------------------------------------------------------
+%% API
+%% ---------------------------------------------------------------------------
 
-build_kafka_message(Bucket, Key, Object) ->
-    Message = {[
-      {<<"action">>, get_action(Object)},
-      {<<"bucket">>, Bucket},
-      {<<"key">>,    Key},
-      {<<"value">>,  get_value(Object)}
-    ]},
-    json:encode(Message).
+postcommit_hook(Object) ->
+    Action = get_action(Object),
+    Bucket = get_bucket(Object),
+    Key = get_key(Object),
+    Value = get_value(Object),
+    ok = call_to_remote_node(Action, Bucket, Key, Value).
+
+call_to_remote_node(Action, Bucket, Key, Value) ->
+    %% gen_server:call({kafka_riak_commitlog, 'commitlog_1@127.0.0.1'}, {produce, <<"store">>, <<"transactions">>, <<"key">>, <<"value for today">>}).
+    Remote = {?OTP_PROCESS, remote_node()},
+    Body = {produce, Action, Bucket, Key, Value},
+    gen_server:call(Remote, Body).
+
+cast_to_remote_node(Action, Bucket, Key, Value) ->
+    %% gen_server:cast({kafka_riak_commitlog, 'commitlog_1@127.0.0.1'}, {produce, <<"store">>, <<"transactions">>, <<"key">>, <<"value for today">>}).
+    Remote = {?OTP_PROCESS, remote_node()},
+    Body = {produce, Action, Bucket, Key, Value},
+    gen_server:cast(Remote, Body).
+
+
+%% ---------------------------------------------------------------------------
+%% Internal
+%% ---------------------------------------------------------------------------
 
 get_action(Object) ->
-  Metadata = riak_object:get_metadata(Object),
-  case dict:find(<<"X-Riak-Deleted">>, Metadata) of
-    {ok, "true"} -> delete;
-    _ -> store
-  end.
+    Metadata = riak_object:get_metadata(Object),
+    case dict:find(<<"X-Riak-Deleted">>, Metadata) of
+        {ok, "true"} -> delete;
+        _ -> store
+    end.
 
 get_bucket(Object) ->
-  riak_object:bucket(Object).
+    riak_object:bucket(Object).
 
 get_key(Object) ->
-  riak_object:key(Object).
+    riak_object:key(Object).
 
 get_value(Object) ->
-  riak_object:get_value(Object).
+    riak_object:get_value(Object).
 
-produce_to_kafka(Key, Message) ->
-  brod:produce_sync(?CLIENTNAME, ?TOPIC, ?PARTITION, Key, Message).
-
-log_output(Bucket, Key, Status, Info) ->
-  LogFile = log_file(Status),
-  Timestamp = iso8601_timestamp(erlang:localtime()),
-  Line = io_lib:format("~p,~p,~p,~p,~p~n", [Timestamp, Bucket, Key, Status, Info]),
-  file:write_file(LogFile, Line, [append]).
-
-log_file(ok) ->
-    "/tmp/kafka_riak_commitlog_metrics.log";
-log_file(_) ->
-    "/tmp/kafka_riak_commitlog_errors.log".
-
-iso8601_timestamp({{Year, Month, Day}, {Hour, Min, Sec}}) ->
-  Format = "~4.10.0B-~2.10.0B-~2.10.0BT~2.10.0B:~2.10.0B:~2.10.0BZ",
-  FormattedIso = io_lib:format(Format, [Year, Month, Day, Hour, Min, Sec]),
-  list_to_binary(FormattedIso).
+remote_node() ->
+    [Name, IP] = string:tokens(atom_to_list(node()), "@"),
+    [_, _, N] = string:tokens(Name, "_"),
+    Remote = "commitlog_" ++ N ++ "@" ++ IP,
+    list_to_atom(Remote).
